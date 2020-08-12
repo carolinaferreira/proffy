13b0cf143d4ce38d8bd8210904a9df56e76e6a9f
carolinaferreira <carol.mf14@gmail.com>
2020-08-12 00:01:04 -0300

    :sparkles: feature to create connection when a student conteact teacher via whatsapp

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,31 +1,52 @@
 import React from 'react';
 import './styles.css';
+import api from '../../services/api';
 const whatsappIcon = require("../../assets/images/icons/whatsapp.svg") as string;
 
-function TeacherItem(){
+
+export interface Teacher{
+    id: number;
+    avatar: string;
+    bio:string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps{
+    teacher:Teacher;
+}
+
+const TeacherItem:React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection(){
+        api.post('connections',{
+            user_id:teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
                     <header>
-                        <img src="https://avatars3.githubusercontent.com/u/17866549?s=460&u=2ecc807c86d99b0debf0664824f303627bde04d6&v=4" alt="Carolina Ferreira"/>
+                        <img src={teacher.avatar} alt={teacher.name}/>
                         <div>
-                            <strong>Carolina Ferreira</strong>
-                            <span>Química</span>
+                            <strong>{teacher.name}</strong>
+                            <span>{teacher.subject}</span>
                         </div>
                     </header>
-                    <p>
-                    Entusiasta das melhores tecnologias de química avançada.
-                    <br/> <br/>
-                    Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-                    </p>
+                    <p>{teacher.bio}</p>
                     <footer>
                         <p>
                             Preço/hora
-                            <strong>R$ 100,00</strong>
+                            <strong>R$ {teacher.cost}</strong>
                         </p>
-                        <button type="button">
+                        <a 
+                            target="_blank"
+                            onClick={createNewConnection} 
+                            href={`https://wa.me/${teacher.whatsapp}`}>
                             <img src={whatsappIcon} alt="WhatsApp"/>
                             Entrar em contato
-                        </button>
+                        </a>
                     </footer>
                 </article>
     )
