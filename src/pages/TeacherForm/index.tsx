6aa0b3a0f4b6c54a9aaea3eb0702a3574298374c
carolinaferreira <carol.mf14@gmail.com>
2020-08-11 21:11:58 -0300

    finished teacher form layout

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -35,11 +35,29 @@ function TeacherForm(){
                 </fieldset>
                 <fieldset>
                     <legend>
-                        Horário disponíveis
+                        Horários disponíveis
                         <button type="button">
                             + Novo horário
                         </button>
                     </legend>
+
+                    <div className="schedule-item">
+                    <Select 
+                        name="week_day" 
+                        label="Dia da semana"
+                        options={[
+                            { value:'0', label:'Domingo' },
+                            { value:'1', label:'Segunda-feira' },
+                            { value:'2', label:'Terça-feira' },
+                            { value:'3', label:'Quarta-feira' },
+                            { value:'4', label:'Quinta-feira' },
+                            { value:'5', label:'Sexta-feira' },
+                            { value:'6', label:'Sábado' },
+                        ]}
+                    />
+                    <Input name="from" label="Das" type="time"/>
+                    <Input name="to" label="Até " type="time"/>
+                    </div>
                 </fieldset>
                 <footer>
                     <p>
